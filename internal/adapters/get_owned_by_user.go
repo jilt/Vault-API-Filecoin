743c4950bf5a1d2053da8a857b834bf3d98abf21
Laura Camellini <jilt@jeeltcraft.com>
2022-04-22 15:19:57 +0200

    update dependencies

--- internal/adapters/get_owned_by_user.go
+++ internal/adapters/get_owned_by_user.go
@@ -8,7 +8,7 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/MalukiMuthusi/mintbase/internal/models"
+	"github.com/jilt/Vault-API-Filecoin/tree/main/internal/models"
 )
 
 func GetOwnedByUser(user *models.UserIDParameter) ([]byte, error) {
